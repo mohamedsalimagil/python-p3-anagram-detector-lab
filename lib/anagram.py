80f77017c25288c70c597fad647cb6333b48f421
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-09-26 22:29:53 +0300

    python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,28 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        matches = []
+        sorted_word = sorted(self.word)  # we can reuse this
+
+        for candidate in candidates:
+            cand_lower = candidate.lower()
+
+            # skip identical words
+            if cand_lower == self.word:
+                continue
+
+            # check if anagram
+            if sorted(cand_lower) == sorted_word:
+                matches.append(candidate)
+
+        return matches
+
+
+# test run
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
+
+
